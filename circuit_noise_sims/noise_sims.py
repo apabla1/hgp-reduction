1cc9c706c99811d6bf55d23ab0cc2d07bd6ff418
Aarav <apabla@ucla.edu>
2025-12-21 03:35:15 -0500

    Instructions for reduced-code transformation

--- circuit_noise_sims/noise_sims.py
+++ circuit_noise_sims/noise_sims.py
@@ -1,6 +1,6 @@
 from reduction_funcs import get_reduced_random_code
 from hgp_tannergraphs import XZ_ctype_tannergraph
-from tannergraph_to_CNOT_circuit import generate_synd_circuit, generate_full_circuit
+from tannergraph_to_CNOT_circuit import generate_full_circuit_split, generate_synd_circuit, generate_full_circuit
 from bposd.css import css_code
 
 if __name__ == '__main__':
@@ -8,22 +8,28 @@ if __name__ == '__main__':
     ### Reduced HGP from random (d_v, d_c), [n, k, d_min] classical code
     ### params: (n, d_v, d_c, d_min, min # of color groups)
     Hx1, Hx2, Hz1, Hz2, code = get_reduced_random_code(20, 3, 5, 6, 5)
-    code1 = css_code(hx=Hx1, hz=Hz1)
-    code2 = css_code(hx=Hx2, hz=Hz2) # will be an invalid code
-    
-    # params: (code, rounds, p1, p2, p_spam, seed)
+
+    # params: (Hx1, Hx2, Hz1, Hz2, rounds, p1, p2, p_spam, seed, measure_x)
     rounds = 2
-    c1 = generate_full_circuit(code1, rounds, 1e-3, 1e-2, 1e-3, 1234)
-    c2 = generate_full_circuit(code2, rounds, 1e-3, 1e-2, 1e-3, 1234)
-    
-    # sample
+    c = generate_full_circuit_split(Hx1, Hx2, Hz1, Hz2, rounds, 1e-3, 1e-2, 1e-3, 1234, measure_x=False)
+   
+    # sample 
     shots = 200
     samples = c.compile_sampler().sample(shots)
-    
-    mz = code.hz.shape[0]
-    n = code.hx.shape[1]
-    
-    # first rounds*mz bits are Z-check outcomes
-    z_synd = samples[:, :rounds*mz].reshape(shots, rounds, mz)
-    
-    final_data = samples[:, rounds*mz: rounds*mz + n]
+
+    mz = Hz1.shape[0]
+    n = Hx1.shape[1]
+
+    # measurement layout (when measure_x=False):
+    # per round:
+    #   Z1: mz bits
+    #   Z2: mz bits
+    #   X stages: no recorded bits
+    # after rounds: final data MR gives n bits
+
+    z1_bits = samples[:, :rounds*mz].reshape(shots, rounds, mz)
+    z2_bits = samples[:, rounds*mz:2*rounds*mz].reshape(shots, rounds, mz)
+
+    z_eff = z1_bits ^ z2_bits
+
+    final_data = samples[:, 2*rounds*mz : 2*rounds*mz + n]
